d9a48cb4723295299412d288207e3f65e5be1ce8
rameem2003 <rameem2019@gmail.com>
2022-03-20 21:40:41 +0600

    some bug fixed

--- app.js
+++ app.js
@@ -4,18 +4,38 @@ function clock(){
     var hour = date.getHours();
     var minute = date.getMinutes();
     var second = date.getSeconds();
+    var timeFormate = document.getElementById("timeFormate");
+
+
+    // if block for when the time is 12 am & 12pm
+    
+    if(hour == 0){
+        hour = 12;
+        timeFormate.innerHTML = "AM"
+    }
+    if(hour > 12){
+        hour = hour - 12;
+        timeFormate.innerHTML = "PM";
+    }
+
+
+    // if block for when the hour, minute & second value is less than 10
+
+
+    if(hour < 10){
+        hour = '0' + hour;
+    }
+
+    if(minute < 10){
+        minute = '0' + minute;
+    }
+
+    if(second < 10){
+        second = '0' + second;
+    }
+
 
     document.getElementById("time").innerHTML = `${hour} : ${minute} : ${second}`;
-    var day = document.getElementById("day");
-
-    if(hour < 12){
-        day.innerHTML = "AM";
-    }
-    
-    else if(hour => 12){
-        hour = hour - 12;
-        day.innerHTML = "PM";
-    }
 
     setInterval(clock, 1000);
 
